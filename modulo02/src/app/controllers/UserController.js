452c205cc70a0fb0927976f195e2bf7820fa2dd0
Flavio <flavio.danilo022@gmail.com>
2019-11-05 23:40:52 -0300

    Inserting Method to update data of User

--- modulo02/src/app/controllers/UserController.js
+++ modulo02/src/app/controllers/UserController.js
@@ -13,5 +13,29 @@ class UserController {
     const { id, name, email, provider } = await Users.create(req.body);
     return res.json({ id, name, email, provider });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+    const user = await Users.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await Users.findOne({
+        where: { email: req.body.email }
+      });
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+    const { id, name, provider } = await user.update(req.body);
+    return res.json({
+      id,
+      name,
+      email,
+      provider
+    });
+  }
 }
 export default new UserController();
--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authHeader from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+routes.use(authHeader);
+routes.put('/users', UserController.update);
 export default routes;
